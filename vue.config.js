285806069a7361616fa36e47185eeba742c26e6b
César Alberca <cesalberca@gmail.com>
2019-04-10 17:57:41 +0200

    Add public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 module.exports = {
+  publicPath: 'blog',
   outputDir: path.resolve(__dirname, './docs'),
   configureWebpack: config => {
     if (process.env.NODE_ENV === 'development') {

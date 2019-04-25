0bc4f474d8d9e8a38b29c003ac8e21869bc4e7d9
César Alberca <cesalberca@gmail.com>
2019-04-25 18:39:07 +0200

    Make VueStateManager observable

--- src/ui/state/VueStateManager.ts
+++ src/ui/state/VueStateManager.ts
@@ -1,10 +1,11 @@
 import { VueConstructor } from 'vue'
-import { StateManager } from '../../application/state/StateManager'
-import { State } from '../../application/state/State'
+import { StateManager, State } from '../../application/state'
+import { Observer } from '../../infraestructure'
 
 export class VueStateManager {
   private static _instance: VueStateManager | null = null
   private _state: State = new State()
+  private readonly stateManager: StateManager = new StateManager()
 
   public static get instance(): VueStateManager {
     if (this._instance === null) {
@@ -14,10 +15,13 @@ export class VueStateManager {
   }
 
   public create(vue: VueConstructor) {
-    const stateManager = new StateManager()
-    const state = stateManager.state
+    const state = this.stateManager.state
     this._state = vue.observable(state)
     return this
+  }
+
+  public register(observer: Observer) {
+    this.stateManager.register(observer)
   }
 
   get state(): State {

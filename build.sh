a10ebfc1611a1d20a72df5333b1c2762c709984d
César Alberca <cesalberca@gmail.com>
2019-05-18 21:17:42 +0200

    Deploy dist

--- build.sh
+++ build.sh
@@ -11,8 +11,12 @@ function deploy() {
   yarn ci
   yarn build
 
+  git add .
+  git commit -m "[CI] Automatic commit"
+
+  git subtree split --branch deploy --prefix dist/
+
   datetime="$(date '+%Y%m%d.%H%M%S')"
-  git subtree push --prefix dist origin gh-pages
   tag_name="${datetime}"
   git tag --annotate "${tag_name}" --message="Deployed on ${datetime}"
   git push origin "${tag_name}" --no-verify

08fd0b9b764d93bcafe52fdc874003fb75878f65
César Alberca <cesalberca@gmail.com>
2019-11-18 23:51:50 +0100

    Add analytics

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import { Application, Router, XApp } from './ui'
 import VueRouter from 'vue-router'
+import VueAnalytics from 'vue-analytics'
 
-new Application(Vue, Router.create(VueRouter)).create(XApp)
+new Application(Vue, Router.create(VueRouter), VueAnalytics).create(XApp)
--- src/ui/Application.ts
+++ src/ui/Application.ts
@@ -10,13 +10,19 @@ import { VueConstructor } from 'vue'
 import { Router } from './Router'
 import VueRouter from 'vue-router'
 import { ServiceWorkerRegisterer } from './ServiceWorkerRegisterer'
+import VueAnalytics from 'vue-analytics'
 
 export class Application {
-  constructor(private readonly vue: VueConstructor, private readonly router: Router) {}
+  constructor(
+    private readonly vue: VueConstructor,
+    private readonly router: Router,
+    private readonly analytics: VueAnalytics
+  ) {}
 
   create(app: VueConstructor) {
     this.vue.use(VueRouter)
     this.vue.config.productionTip = false
+    this.vue.use(this.analytics, { id: 'UA-66392350-1' })
     ServiceWorkerRegisterer.create()
 
     new this.vue({

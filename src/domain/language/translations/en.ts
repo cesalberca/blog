393ee4d7cb89fa838aee102375eb18027091069e
César Alberca <cesalberca@gmail.com>
2020-01-12 20:28:04 +0100

    Update about me section

--- src/domain/language/translations/en.ts
+++ src/domain/language/translations/en.ts
@@ -21,7 +21,7 @@ export const en: Translation = new Map<keyof TranslationIdentifiers, string>([
   ['about_title', 'About me'],
   [
     'about_description',
-    "I'm a young programmer very interested in good practices, architecture, testing and SOLID principles. I work at [Autentia](https://www.autentia.com/) as a frontend developer (Vue, React, TypeScript, JavaScript, HTML, CSS, etc) although I have worked a bit in Java and Android. I'm continuously on the lookout for things to read, learn and [share](https://www.adictosaltrabajo.com/author/calberca/). I've talked at [JSDay Madrid](https://www.youtube.com/watch?v=aNf1Oos0ZB8&t=1s), Codenares 2018 and 2019, [Ritsi](https://www.youtube.com/watch?v=SuykoyC0T74&t=1s) and several internal talks at my company, like [this one](https://www.youtube.com/watch?v=RFQy-ud8Ec4&t=4477s)."
+    "I'm a software developer very interested in good practices, architecture and testing. I work at [Autentia](https://www.autentia.com/) as a frontend developer with technologies such as React, Vue, Angular, TypeScript, JavaScript, HTML, CSS, etc. I'm continuously on the lookout for things to read, learn and [share](https://www.adictosaltrabajo.com/author/calberca/). I've talked at [JSDay Madrid 2019](https://www.youtube.com/watch?v=aNf1Oos0ZB8&t=1s), Codenares 2018 and 2019, [Ritsi](https://www.youtube.com/watch?v=SuykoyC0T74&t=1s), [JSDay Canarias 2019](https://www.youtube.com/watch?v=3nPPRvRcoo0&feature=youtu.be) and [Codemotion Madrid 2019](https://www.youtube.com/watch?v=y0BymiCki54&feature=youtu.be)."
   ],
   ['talks_title', 'Talks'],
   ['talks_talkTitle', 'Title'],
--- src/domain/language/translations/es.ts
+++ src/domain/language/translations/es.ts
@@ -21,7 +21,7 @@ export const es: Translation = new Map<keyof TranslationIdentifiers, string>([
   ['about_title', 'Acerca de mí'],
   [
     'about_description',
-    "I'm a young programmer very interested in good practices, architecture, testing and SOLID principles. I work at [Autentia](https://www.autentia.com/) as a frontend developer (Vue, React, TypeScript, JavaScript, HTML, CSS, etc) although I have worked a bit in Java and Android. I'm continuously on the lookout for things to read, learn and [share](https://www.adictosaltrabajo.com/author/calberca/). I've talked at [JSDay Madrid](https://www.youtube.com/watch?v=aNf1Oos0ZB8&t=1s), Codenares 2018 and 2019, [Ritsi](https://www.youtube.com/watch?v=SuykoyC0T74&t=1s) and several internal talks at my company, like [this one](https://www.youtube.com/watch?v=RFQy-ud8Ec4&t=4477s)."
+    'Soy un desarrollador interesado en buenas prácticas, arquitectura y testing. Trabajo en [Autentia](https://www.autentia.com/) como desarrollador frontend con tecnologías como React, Vue, Angular, TypeScript, JavaScript, HTML, CSS, etc. Siempre estoy en busca de cosas que leer, aprender y compartir. He dado charlas en [JSDay Madrid](https://www.youtube.com/watch?v=aNf1Oos0ZB8&t=1s), Codenares 2018 and 2019, [Ritsi](https://www.youtube.com/watch?v=SuykoyC0T74&t=1s), [JSDay Canarias 2019](https://www.youtube.com/watch?v=3nPPRvRcoo0&feature=youtu.be) y [Codemotion Madrid 2019](https://www.youtube.com/watch?v=y0BymiCki54&feature=youtu.be).'
   ],
   ['talks_title', 'Charlas'],
   ['talks_talkTitle', 'Title'],

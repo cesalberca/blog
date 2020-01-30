5d31715bead5bf06b7c22e9e33f9867ae7f5b42d
César Alberca <cesalberca@gmail.com>
2020-01-30 20:42:08 +0100

    Add basic nuxt configuration

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  moduleNameMapper: {
+    '^vue$': 'vue/dist/vue.common.js'
+  },
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
   setupFiles: ['./tests/set-up.ts'],
   coverageReporters: ['lcov', 'text-summary'],
-  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)']
+  moduleFileExtensions: ['js', 'vue', 'json'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/components/**/*.vue', '<rootDir>/pages/**/*.vue']
 }

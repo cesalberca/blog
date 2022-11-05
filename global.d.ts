f516401ff488f5c0c31df6d17722ed6383d94cd4
cesalberca <cesalberca@gmail.com>
2022-11-05 17:27:11 +0000

    Add type declarations for svg

--- global.d.ts
+++ global.d.ts
@@ -1,2 +1,7 @@
 type Messages = typeof import('./src/core/i18n/en.json')
 declare interface IntlMessages extends Messages {}
+
+declare module '*.svg' {
+  const content: string
+  export default content
+}

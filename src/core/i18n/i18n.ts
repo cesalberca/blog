8904ae870c13e272e3aa71bbd01001abebe8b2a5
cesalberca <cesar@cesalberca.com>
2024-07-16 18:55:30 +0200

    Remove comment

--- src/core/i18n/i18n.ts
+++ src/core/i18n/i18n.ts
@@ -5,7 +5,6 @@ import { getRequestConfig } from 'next-intl/server'
 const locales = ['en', 'es']
 
 export default getRequestConfig(async ({ locale }) => {
-  // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound()
 
   return {

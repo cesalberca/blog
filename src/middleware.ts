433c6ea682a76136068da148bd53d06f837bcf89
cesalberca <cesar@cesalberca.com>
2024-07-16 18:38:51 +0200

    Move into single middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,10 @@
-export { i18nMiddleware as default, config } from './i18n.middleware'
+import createMiddleware from 'next-intl/middleware'
+
+export default createMiddleware({
+  locales: ['en', 'de'],
+  defaultLocale: 'en',
+})
+
+export const config = {
+  matcher: ['/', '/(en|es)/:path*'],
+}

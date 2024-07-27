64b8aef7730853ecbe1ede777bdaca3d05d23344
cesalberca <cesar@cesalberca.com>
2024-07-27 16:12:56 +0300

    Fix markdown

--- src/core/components/markdown/markdown.tsx
+++ src/core/components/markdown/markdown.tsx
@@ -1,6 +1,6 @@
 import type { FC } from 'react'
-import { Markdown as MarkdownDomain } from '../../markdown'
+import ReactMarkdown from 'react-markdown'
 
-export const Markdown: FC<{ value: string }> = ({ value }) => {
-  return <div dangerouslySetInnerHTML={{ __html: MarkdownDomain.fromValue(value).value }}></div>
+export const Markdown: FC<{ value: string }> = async ({ value }) => {
+  return <ReactMarkdown>{value}</ReactMarkdown>
 }
--- src/core/markdown.ts
+++ src/core/markdown.ts
@@ -1,7 +1,13 @@
+import { serialize } from 'next-mdx-remote/serialize'
+
 export class Markdown {
   constructor(readonly value: string) {}
 
   static fromValue(value: string) {
     return new Markdown(value)
+  }
+
+  toHtml() {
+    return serialize(this.value)
   }
 }

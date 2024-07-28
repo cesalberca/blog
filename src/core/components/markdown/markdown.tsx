d456a17d9d6f42bd82e3ffaac0b52506d11b8bd6
cesalberca <cesar@cesalberca.com>
2024-07-28 17:41:46 +0300

    Add syntax highlighting

--- src/core/components/markdown/markdown.tsx
+++ src/core/components/markdown/markdown.tsx
@@ -1,6 +1,28 @@
 import type { FC } from 'react'
+import { Prism as SyntaxHighlighter, type SyntaxHighlighterProps } from 'react-syntax-highlighter'
+import dark from 'react-syntax-highlighter/dist/esm/styles/prism/synthwave84'
 import ReactMarkdown from 'react-markdown'
 
 export const Markdown: FC<{ value: string }> = async ({ value }) => {
-  return <ReactMarkdown>{value}</ReactMarkdown>
+  return (
+    <ReactMarkdown
+      components={{
+        code(props) {
+          const { children, className, node, ...rest } = props
+          const match = /language-(\w+)/.exec(className || '')
+          return match ? (
+            <SyntaxHighlighter {...(rest as SyntaxHighlighterProps)} PreTag="div" language={match[1]} style={dark}>
+              {String(children).replace(/\n$/, '')}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...rest} className={className}>
+              {value}
+            </code>
+          )
+        },
+      }}
+    >
+      {value}
+    </ReactMarkdown>
+  )
 }

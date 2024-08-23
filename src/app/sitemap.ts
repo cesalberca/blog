a3e36cee43e59d813c3b1aecf58f89a20d9aeeda
cesalberca <cesar@cesalberca.com>
2024-08-23 11:02:43 +0300

    Remove generateMetadata

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,12 +3,12 @@ import { getPosts } from '@/posts'
 export const baseUrl = 'https://cesalberca.com'
 
 export default async function sitemap() {
-  let blogs = (await getPosts()).map(post => ({
+  const blogs = (await getPosts()).map(post => ({
     url: `${baseUrl}/blog/${post.slug}`,
     lastModified: post.date,
   }))
 
-  let routes = ['', '/blog'].map(route => ({
+  const routes = ['', '/blog'].map(route => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
--- src/app/talks/mdx-layout.tsx
+++ src/app/talks/mdx-layout.tsx
@@ -1,40 +1,6 @@
-import { Page } from '@/core/components/page/page'
-import { LeetBackground } from '@/core/components/leet-card/leet-background'
-import { ScrambleText } from '@/core/components/scramble-text/scramble-text'
-import type { PropsWithChildren, ReactNode } from 'react'
+import type { ReactNode } from 'react'
 import type { TalkMetadata } from '@/talks'
-import { baseUrl } from '@/app/sitemap'
 import { TalkPage } from '@/features/talks/delivery/talk.page'
-
-/*
-export async function generateMetadata({ params }: Params): Promise<Metadata | void> {
-  const { title, date: publishedTime, image, summary } = params.metadata
-  const ogImage = image ? image : `${baseUrl}/og?title=${encodeURIComponent(title)}`
-
-  return {
-    title,
-    description: summary,
-    openGraph: {
-      title,
-      description: summary,
-      type: 'article',
-      publishedTime,
-      url: `${baseUrl}/blog/${params.slug}`,
-      images: [
-        {
-          url: ogImage,
-        },
-      ],
-    },
-    twitter: {
-      card: 'summary_large_image',
-      title,
-      description: summary,
-      images: [ogImage],
-    },
-  }
-}
-*/
 
 export default async function MdxLayout({ children, slug }: { children: ReactNode; slug: string }) {
   const { metadata } = (await import(`./(talks)/${slug}/page.mdx`)) as { metadata: TalkMetadata }

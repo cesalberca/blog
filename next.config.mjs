d39a7804d92124aece4729076d462ed1d3af13ff
cesalberca <cesar@cesalberca.com>
2024-11-11 12:42:31 +0100

    Add redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 import createNextIntlPlugin from 'next-intl/plugin'
 import withMDX from '@next/mdx'
 
-
 const withNextIntl = createNextIntlPlugin('./src/core/i18n/i18n.tsx')
 
 /** @type {import('next').NextConfig} */
@@ -9,6 +8,10 @@ const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   async redirects() {
     return [
+      {
+        source: '/chat',
+        destination: 'https://calendar.app.google/KvZAYd9okNjiY7iBA',
+      },
       {
         source: '/articles/:path*',
         destination: '/blog/:path*',

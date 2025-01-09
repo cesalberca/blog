afa7c3ec60d66bdb05acb806c43949a8611051fc
cesalberca <cesar@cesalberca.com>
2025-01-09 18:06:27 +0100

    Add link

--- src/core/components/markdown/markdown.tsx
+++ src/core/components/markdown/markdown.tsx
@@ -3,14 +3,14 @@ import { Prism as SyntaxHighlighter, type SyntaxHighlighterProps } from 'react-s
 import dark from 'react-syntax-highlighter/dist/esm/styles/prism/synthwave84'
 import ReactMarkdown from 'react-markdown'
 import { cn } from '@/lib/utils'
+import { Link } from '@/core/components/link/link'
 
 export const Markdown: FC<{ value: string; className?: string }> = async ({ value, className }) => {
   return (
     <ReactMarkdown
       className={cn('prose prose-zinc dark:prose-invert text-current', className)}
       components={{
-        code(props) {
-          const { children, className, ...rest } = props
+        code({ children, className, ...rest }) {
           const match = /language-(\w+)/.exec(className ?? '')
           return match ? (
             <SyntaxHighlighter {...(rest as SyntaxHighlighterProps)} PreTag="div" language={match[1]} style={dark}>
@@ -18,9 +18,12 @@ export const Markdown: FC<{ value: string; className?: string }> = async ({ valu
             </SyntaxHighlighter>
           ) : (
             <code {...rest} className={className}>
-              {value}
+              {children}
             </code>
           )
+        },
+        a({ href, children }) {
+          return <Link to={href ?? ''}>{children}</Link>
         },
       }}
     >

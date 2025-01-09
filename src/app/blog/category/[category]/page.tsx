cea6eae08e26474856dfb4c06aaff4a73f403313
cesalberca <cesar@cesalberca.com>
2025-01-09 14:14:46 +0100

    Fix lint errors

--- src/app/blog/category/[category]/page.tsx
+++ src/app/blog/category/[category]/page.tsx
@@ -1,9 +1,22 @@
 import type { Category } from '@/app/category'
 import { categories } from '@/app/categories'
 import { CategoryPage } from '@/features/posts/delivery/category.page'
+import { notFound } from 'next/navigation'
 
-export default async function Index({ params }: { params: { category: Category } }) {
-  return <CategoryPage category={params.category} />
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type Params = {
+  params: {
+    category: Category
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function Index({ params }: any) {
+  const { category } = params
+
+  if (categories.indexOf(category) == -1) notFound()
+
+  return <CategoryPage category={category} />
 }
 
 export async function generateStaticParams() {

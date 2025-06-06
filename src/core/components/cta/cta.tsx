ed72aceddcbbddf64df7d181d6596aaa823d8cce
cesalberca <cesar@cesalberca.com>
2025-06-06 15:41:00 +0200

    Improve CTA

--- src/core/components/cta/cta.tsx
+++ src/core/components/cta/cta.tsx
@@ -13,7 +13,7 @@ interface CTAProps {
 export const CTA: FC<CTAProps> = ({ title, description, buttonText, buttonLink, className = '' }) => {
   return (
     <div className={`bg-accent p-6 rounded-lg my-8 ${className}`}>
-      <h3 className="text-xl font-bold mb-2">{title}</h3>
+      <h3 className="text-xl font-bold mb-2 mt-2">{title}</h3>
       <p className="mb-4">{description}</p>
       <Button asChild>
         <Link type="invisible" to={buttonLink}>

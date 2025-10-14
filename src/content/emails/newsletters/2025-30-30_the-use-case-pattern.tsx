268faa5c7f3c0e168e2129d99e015abc918e5ed8
cesalberca <cesar@cesalberca.com>
2025-10-14 16:59:53 +0200

    Add new components

--- src/content/emails/newsletters/2025-30-30_the-use-case-pattern.tsx
+++ src/content/emails/newsletters/2025-30-30_the-use-case-pattern.tsx
@@ -1,28 +1,87 @@
-import type { FC } from 'react'
+import { createElement, type FC } from 'react'
 import { NewsletterTemplate } from '@/emails/newsletter-template'
 import type { NewsletterMetadata } from '@/features/email/domain/newsletter-metadata'
 import path from 'path'
 import * as runtime from 'react/jsx-runtime'
-import { evaluateSync, type EvaluateOptions } from '@mdx-js/mdx'
+import { type EvaluateOptions, evaluateSync } from '@mdx-js/mdx'
 import fs from 'node:fs'
 import { CodeBlock } from '@/features/email/delivery/code-block/code-block'
 import type { PrismLanguage } from '@react-email/code-block'
-import { CodeInline } from '@react-email/components'
+import { CodeInline, Text } from '@react-email/components'
+import { Link } from '@/features/email/delivery/link/link'
 
 export const metadata: NewsletterMetadata = {
   subject: 'The Hemingway Bridge',
 }
 
-export const WelcomeNewsletter: FC = () => {
-  const file = path.join(process.cwd(), 'src', 'content', 'emails', 'newsletters', `_the-use-case-pattern.mdx`)
+function slugify(str: string) {
+  return str
+    .toString()
+    .toLowerCase()
+    .trim() // Remove whitespace from both ends of a string
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w-]+/g, '') // Remove all non-word characters except for -
+    .replace(/--+/g, '-') // Replace multiple - with single -
+}
 
-  const code = fs.readFileSync(file, { encoding: 'utf8' })
+function createHeading(level: number) {
+  const Heading = ({ children }: { children: string }) => {
+    const slug = slugify(children)
+    return createElement(
+      `h${level}`,
+      { id: slug, className: 'text-white' },
+      [
+        createElement('a', {
+          href: `#${slug}`,
+          key: `link-${slug}`,
+          className: 'anchor',
+        }),
+      ],
+      children,
+    )
+  }
+
+  Heading.displayName = `Heading${level}`
+
+  return Heading
+}
+
+const file = path.join(process.cwd(), 'src', 'content', 'emails', 'newsletters', `_the-use-case-pattern.mdx`)
+
+const code = fs.readFileSync(file, { encoding: 'utf8' })
+
+export const WelcomeNewsletter: FC = () => {
   const { default: Content } = evaluateSync(code, {
     ...(runtime as Readonly<EvaluateOptions>),
     useMDXComponents: () => ({
+      h1: createHeading(1),
+      h2: createHeading(2),
+      h3: createHeading(3),
+      h4: createHeading(4),
+      h5: createHeading(5),
+      h6: createHeading(6),
+      strong: props => {
+        return <strong className="text-white">{props.children}</strong>
+      },
+      a: props => {
+        return <Link {...props}></Link>
+      },
+      blockquote: props => {
+        return (
+          <blockquote
+            {...props}
+            className="bg-muted text-white border-l-4 border-border border-solid rounded-lg p-2 pl-4 m-0 border-t-0 border-b-0 border-r-0"
+          ></blockquote>
+        )
+      },
+      p: props => {
+        return <Text className="text-lg" {...props}></Text>
+      },
       code: props => {
-        console.log({ props })
-        return <CodeInline>{props.children}</CodeInline>
+        return (
+          <CodeInline className="text-white py-1 px-2 bg-secondary rounded-md font-mono">{props.children}</CodeInline>
+        )
       },
       pre: props => {
         const codeContent = props.children?.props?.children ?? ''

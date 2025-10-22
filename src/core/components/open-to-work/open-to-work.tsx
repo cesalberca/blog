6c58923378e7edc0d5090bb7f1d8b46fe0563c69
cesalberca <cesar@cesalberca.com>
2025-10-22 14:53:15 +0200

    Hide newsletter

--- src/core/components/open-to-work/open-to-work.tsx
+++ src/core/components/open-to-work/open-to-work.tsx
@@ -5,7 +5,7 @@ import { useTranslations } from 'next-intl'
 export const OpenToWork: FC = () => {
   const t = useTranslations()
   return (
-    <div className="fixed z-10 bottom-0 left-0 mx-2 my-4">
+    <div className="fixed z-10 bottom-0 text-foreground left-0 mx-2 my-4">
       <CircleText text={t('common.openToWork')} />
     </div>
   )

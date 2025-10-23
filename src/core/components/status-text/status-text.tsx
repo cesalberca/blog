634fdb9918642ce85e0fbf26299de35066c8fe23
cesalberca <cesar@cesalberca.com>
2025-10-24 00:33:17 +0200

    Add i18n

--- src/core/components/status-text/status-text.tsx
+++ src/core/components/status-text/status-text.tsx
@@ -4,5 +4,5 @@ import { useTranslations } from 'next-intl'
 export const StatusText: FC<{ type: 'soon' | 'new' }> = ({ type }) => {
   const t = useTranslations('common.statusText')
 
-  return <span className="ml-1 text-orange-400 align-super text-[8px]">{t(type)}</span>
+  return <span className="ml-1 text-muted-foreground/90 align-super text-[8px]">{t(type)}</span>
 }

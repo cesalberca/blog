43166179f76ac5f38e0e507202c8ae8cb86307c0
cesalberca <cesar@cesalberca.com>
2025-11-19 14:45:27 +0700

    Rename newsletter

--- src/emails/newsletter/_run-use-cases-using-the-command-pattern.tsx
+++ src/emails/newsletter/_run-use-cases-using-the-command-pattern.tsx
@@ -2,9 +2,9 @@ import { type FC } from 'react'
 import { NewsletterEmailLoader } from '@/features/email/delivery/newsletter-email-loader/newsletter-email-loader'
 
 const FrontendArchitectureNewsletter: FC & { title: string } = () => {
-  return <NewsletterEmailLoader slug={`_run-use-cases-using-chain-of-responsibility-and-middlewares`} />
+  return <NewsletterEmailLoader slug={`_run-use-cases-using-the-command-pattern`} />
 }
 
-FrontendArchitectureNewsletter.title = 'Use Cases, The Chain Of Responsibility Pattern and Middlewares'
+FrontendArchitectureNewsletter.title = 'Run Use Cases Using The Command Pattern'
 
 export default FrontendArchitectureNewsletter

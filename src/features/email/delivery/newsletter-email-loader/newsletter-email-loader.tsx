539adaa22ae32c78a9d7183e54003d83dacd1397
cesalberca <cesar@cesalberca.com>
2025-11-20 12:57:24 +0700

    Use factory

--- src/features/email/delivery/newsletter-email-loader/newsletter-email-loader.tsx
+++ src/features/email/delivery/newsletter-email-loader/newsletter-email-loader.tsx
@@ -1,24 +1,11 @@
-import { createElement, type FC } from 'react'
+import { type FC } from 'react'
 import { NewsletterTemplate } from '@/features/email/delivery/templates/newsletter-template'
 import type { NewsletterMetadata } from '@/features/email/domain/newsletter-metadata'
 import path from 'path'
 import * as runtime from 'react/jsx-runtime'
 import { type EvaluateOptions, evaluateSync } from '@mdx-js/mdx'
 import fs from 'node:fs'
-import { CodeBlock } from '@/features/email/delivery/components/code-block/code-block'
-import type { PrismLanguage } from '@react-email/code-block'
-import { CodeInline, Text } from '@react-email/components'
-import { Link } from '@/features/email/delivery/components/link/link'
-
-function createHeading(level: number) {
-  const Heading = ({ children }: { children: string }) => {
-    return createElement(`h${level}`, { className: 'text-primary' }, children)
-  }
-
-  Heading.displayName = `Heading${level}`
-
-  return Heading
-}
+import { MdxComponentsFactory } from '@/core/mdx/mdx-components-factory'
 
 export const NewsletterEmailLoader: FC<{ slug: string }> = ({ slug }) => {
   const file = path.join(process.cwd(), 'src', 'content', 'emails', 'newsletter', slug, 'en.mdx')
@@ -26,56 +13,7 @@ export const NewsletterEmailLoader: FC<{ slug: string }> = ({ slug }) => {
 
   const { default: Content, metadata } = evaluateSync(code, {
     ...(runtime as Readonly<EvaluateOptions>),
-    useMDXComponents: () => ({
-      h1: createHeading(1),
-      h2: createHeading(2),
-      h3: createHeading(3),
-      h4: createHeading(4),
-      h5: createHeading(5),
-      h6: createHeading(6),
-      ul: props => {
-        return <ul className="text-xl" {...props}></ul>
-      },
-      ol: props => {
-        return <ol className="text-xl" {...props}></ol>
-      },
-      strong: props => {
-        return <strong className="text-primary">{props.children}</strong>
-      },
-      a: props => {
-        let href = props.href
-        if (props.href.startsWith('/')) {
-          href = process.env['NEXT_PUBLIC_URL'] + props.href
-        }
-        return <Link {...props} href={href}></Link>
-      },
-      blockquote: props => {
-        return (
-          <blockquote
-            {...props}
-            className="bg-muted text-muted-foreground border-l-4 border-border border-solid rounded-lg p-2 pl-4 m-0 border-t-0 border-b-0 border-r-0"
-          ></blockquote>
-        )
-      },
-      p: props => {
-        return <Text className="text-lg" {...props}></Text>
-      },
-      code: props => {
-        return (
-          <CodeInline className="text-foreground py-1 px-2 bg-secondary rounded-md font-mono">
-            {props.children}
-          </CodeInline>
-        )
-      },
-      pre: props => {
-        const codeContent = props.children?.props?.children ?? ''
-
-        const className = props.children?.props?.className ?? ''
-        const language = className.startsWith('language-') ? className.replace('language-', '') : 'typescript'
-
-        return <CodeBlock code={codeContent} language={language as PrismLanguage}></CodeBlock>
-      },
-    }),
+    useMDXComponents: () => MdxComponentsFactory.get('email'),
   })
 
   const typedMetadata = metadata as NewsletterMetadata
